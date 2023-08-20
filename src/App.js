163aa8cfd4dd74d3caca376bdfd3e4aa48d0ccf4
AlexVelezLl <alanvele@espol.edu.ec>
2023-08-20 10:55:31 -0500

    Init project architecture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Desde aqui se va a importar environment.js e instanciar el sdk
 function App() {
   return (
     <div className="App">
---
+++ src/services/index.js
@@ -0,0 +1,2 @@
+// Crear una clase para que se instancien los servicios del SDK para productos.
+// Que recoja el environment url y que recoja el authorId
